eaad67863e425e4ff8bd143c32f6a0cd9c213212
Davi Busanello <itsme@davibusanello.net>
2018-09-01 21:18:45 -0300

    Bootstraped the project

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+  app: {
+    token: '099b38a71182465d9a019114b419d2e1', // <- enter your token here
+    muted: false, // <- mute microphone by default
+    googleIt: true, // <- ask users to google their request, in case of input.unknown action
+  },
+  locale: {
+    strings: {
+    },
+    settings: {
+      speechLang: 'pt-BR', // <- output language
+      recognitionLang: 'pt-BR', // <- input(recognition) language
+    },
+  },
+};
