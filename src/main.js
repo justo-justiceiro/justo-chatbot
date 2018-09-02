d9e22dab735dd990be8a3ead0a4eca7c8afdcaf7
Davi Busanello <itsme@davibusanello.net>
2018-09-02 12:54:29 -0300

    Add analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueCarousel from 'vue-carousel';
 import VueMaterial from 'vue-material';
+import VueAnalytics from 'vue-analytics';
 // import 'vue-material/dist/vue-material.min.css';
 import App from './App.vue';
 import router from './router';
@@ -11,6 +12,9 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 Vue.use(VueCarousel);
 Vue.use(VueMaterial);
+Vue.use(VueAnalytics, {
+  id: 'UA-31682808-2',
+});
 
 new Vue({
   router,
