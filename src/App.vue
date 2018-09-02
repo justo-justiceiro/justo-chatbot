b4ff87a2bf55e8349b725e0b8f396586471ae32b
Davi Busanello <itsme@davibusanello.net>
2018-09-02 00:58:12 -0300

    Front v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">Sobre</router-link>
+      <router-link to="/">Home</router-link>
     </div>
-    <!-- <router-view/> -->
+    <router-view/>
     <section id ="app">
 
     <!-- The input -->
