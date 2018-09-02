9de8c54a6c48b2c1f29f7bfd2f166b10de00038d
Davi Busanello <itsme@davibusanello.net>
2018-09-01 22:26:31 -0300

    Add vue-material

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import VueCarousel from 'vue-carousel';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+
 Vue.config.productionTip = false;
+Vue.use(VueCarousel);
+Vue.use(VueMaterial);
 
 new Vue({
   router,
