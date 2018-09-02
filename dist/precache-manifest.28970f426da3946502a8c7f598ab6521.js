b74d34dbd35ef54140f07567d03bcd3c176ad457
Davi Busanello <itsme@davibusanello.net>
2018-09-02 01:58:12 -0300

    Add dist/ to deploy to github pages

---
+++ dist/precache-manifest.28970f426da3946502a8c7f598ab6521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1aa66966b77fa98ac2da",
+    "url": "/js/chunk-vendors.c00a1e6a.js"
+  },
+  {
+    "revision": "2daad4355cf1b75da33f",
+    "url": "/js/app.93ba49ba.js"
+  },
+  {
+    "revision": "6b151d522ad7498b24a71291286aac83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa66966b77fa98ac2da",
+    "url": "/css/chunk-vendors.cecd77e2.css"
+  },
+  {
+    "revision": "2daad4355cf1b75da33f",
+    "url": "/css/app.1ecd7cc8.css"
+  }
+];
