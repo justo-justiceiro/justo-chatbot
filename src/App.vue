7440a9a99593e1225df7d1ac49058b1cd597055a
Davi Busanello <itsme@davibusanello.net>
2018-09-02 01:10:00 -0300

    Add views to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">Sobre</router-link> |
+      <router-link to="/justo">Justo, fale comigo</router-link>
     </div>
     <router-view/>
     <section id ="app">
---
+++ src/views/Justo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="justo">
+    {{helo}}
+    <h1>Olá eu sou Justo, o Justiceiro do Dinheiro</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'justo',
+  components: {
+  },
+};
+</script>
